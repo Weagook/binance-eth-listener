50a8c4f35c7aa6817240104014aa505539a453bb
Weagook <weagook@gmail.com>
2023-03-10 14:58:54 +0600

    adding comments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from binance import AsyncClient, BinanceSocketManager
 from datetime import datetime
 
+# Create variables to store results and time
 first_time = datetime.now()
 first_result = 0
 
@@ -10,16 +11,25 @@ async def kline_listener(client):
     global first_time
     global first_result
 
+    # We create a client
     bm = BinanceSocketManager(client)
+
     async with bm.kline_socket(symbol='ETHUSDT') as stream:
         while True:
             res = await stream.recv()
+            # Choose only closed
             res = float(res['k']['c'])
+
+            # Every time we check how much time has passed
             new_time = datetime.now()
             passed = new_time - first_time
             passed = passed.total_seconds() // 60
+
+            # If 60 minutes have passed, reset the counter
             if passed >= 60:
                 first_time = datetime.now()
+                
+            # If there were changes by 1 percent, we output information to the console and save the last result.
             if res > first_result + first_result * 0.01 or res < first_result - first_result * 0.01:
                 print('-----')
                 print('Changes:')
@@ -28,13 +38,12 @@ async def kline_listener(client):
                 print('-----')
                 first_time = datetime.now()
 
+# Main function
 async def main():
-
     client = await AsyncClient.create()
     await kline_listener(client)
 
-
+# Trigger Condition
 if __name__ == "__main__":
-
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
