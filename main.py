55a15f0eac3fbd23334cf0abf659eca68c48ead7
Weagook <weagook@gmail.com>
2023-03-10 13:28:48 +0600

    complete listener

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import asyncio
-import json
-
 from binance import AsyncClient, BinanceSocketManager
+from datetime import datetime
+
+first_time = datetime.now()
+first_result = 0
+
 
 async def kline_listener(client):
+    global first_time
+    global first_result
+
     bm = BinanceSocketManager(client)
     async with bm.kline_socket(symbol='ETHUSDT') as stream:
         while True:
             res = await stream.recv()
-            print(res['k']['c'])
+            res = float(res['k']['c'])
+            new_time = datetime.now()
+            passed = new_time - first_time
+            passed = passed.total_seconds() // 60
+            if passed >= 60:
+                first_time = datetime.now()
+            if res > first_result + first_result * 0.01 or res < first_result - first_result * 0.01:
+                print('-----')
+                print('Changes:')
+                first_result = res
+                print(first_result)
+                print('-----')
+                first_time = datetime.now()
 
 async def main():
 
